f87e1260568e197235853f04bef5ac9cc02ea594
Shilpa Kundapur <skundapur@dataguard.de>
2020-09-28 13:36:51 +0530

    Implement square root

--- cool_new_feature.py
+++ cool_new_feature.py
@@ -3,3 +3,5 @@ if __name__ == '__main__':
 	num2 = input('Enter second number: ')
 	sum = float(num1) + float(num2)
 	print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+	num = float(input('Enter a number to find the square root: '))
+	print('The square root of %0.3f is %0.3f'%(num ,num ** 0.5))
