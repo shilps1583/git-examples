f116cf55e7e3b29dea7e67301fd0db204ef88933
Shilpa Kundapur <skundapur@dataguard.de>
2020-09-28 13:16:52 +0530

    Third commit

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,4 @@ from datetime import datetime
 if __name__ == '__main__':
 	now = datetime.now()
 	current_time = now.strftime("%H:%M:%S")
-	print('hello world at', current_time)
+	print('hello world. The current time is:', current_time)
